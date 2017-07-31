a5d966884ab442fc28288d340788fc7ae71a9044
Paulo Oliveira <paulo.oliveira@miniclip.com>
2017-07-31 19:32:36 +0100

    Allow for local file update, also
    Separated from within app.src using remote | local

--- src/egeoip_db_updater.erl
+++ src/egeoip_db_updater.erl
@@ -20,37 +20,40 @@
 
 -define(CB_MODULE, ?MODULE).
 -define(REFRESH_INTERVAL_MS, timer:hours(12)).
--define(REFRESH_TIMEOUT_MS, timer:seconds(15)).
+-define(REFRESH_FROM_REMOTE_TIMEOUT_MS, timer:seconds(15)).
 
 %% ------------------------------------------------------------------
 %% Record Definitions
 %% ------------------------------------------------------------------
 
 -record(state, {
+    dblocation :: dblocation(),
     dbpath :: string(),
     db :: binary()
 }).
 -type state() :: #state{}.
 
+-type dblocation() :: local | remote.
+
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
--spec start_link(DbPath :: undefined
+-spec start_link(DbLocAndPath :: undefined
                          | string())
       -> ignore | {ok, pid()}.
 start_link(undefined) -> ignore;
-start_link(DbPath) -> gen_server:start_link(?CB_MODULE, [DbPath], []).
+start_link(DbLocAndPath) -> gen_server:start_link(?CB_MODULE, [DbLocAndPath], []).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
--spec init([DbPath :: string()])
+-spec init([DbLocAndPath :: {dblocation(), string()}])
       -> {ok, State :: state()}.
-init([DbPath]) ->
+init([{DbLocation, DbPath}]) ->
     erlang:send_after(0, self(), reload_db),
-    {ok, #state{ dbpath = DbPath }}.
+    {ok, #state{ dblocation = DbLocation, dbpath = DbPath }}.
 
 -spec terminate(Reason :: term(),
                 State :: state())
@@ -83,18 +86,41 @@ handle_cast(_Request, State) ->
                         | term(),
                   State :: state())
       -> {noreply, NewState :: state()}.
-handle_info(reload_db, #state{ dbpath = DbPath, db = Db } = State) ->
-    NewState
-        = case httpc:request(get, {DbPath, []}, [{timeout, ?REFRESH_TIMEOUT_MS}], []) of
-              {error, _Reason} ->
-                  State;
-              {ok, {_, _, DatGZ}} when DatGZ =/= Db ->
-                  _ = egeoip:reload({compressed_data, DatGZ}),
-                  State#state{ db = DatGZ };
-              {ok, _} ->
-                  State
-          end,
+handle_info(reload_db, #state{ dblocation = DbLoc, dbpath = DbPath } = State) ->
+    NewState = maybe_reload_db_from_location(DbLoc, DbPath, State),
     erlang:send_after(?REFRESH_INTERVAL_MS, self(), reload_db),
     {noreply, NewState};
 handle_info(_Info, State) ->
     {noreply, State}.
+
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------
+
+-spec maybe_reload_db_from_location(DbLoc :: dblocation(),
+                                    DbPath :: string(),
+                                    State :: state())
+      -> NewState :: state().
+maybe_reload_db_from_location(remote, DbPath, State) ->
+    case httpc:request(get, {DbPath, []}, [{timeout, ?REFRESH_FROM_REMOTE_TIMEOUT_MS}], []) of
+        {error, _Reason} ->
+            State;
+        {ok, {_, _, DatGZ}} ->
+            maybe_reload_db(DatGZ, State)
+    end;
+maybe_reload_db_from_location(local, DbPath, State) ->
+    case file:read_file(DbPath) of
+        {error, _Reason} ->
+            State;
+        {ok, DatGZ} ->
+            maybe_reload_db(DatGZ, State)
+    end.
+
+-spec maybe_reload_db(DatGZ :: binary(),
+                      State :: state())
+      -> NewState :: state().
+maybe_reload_db(DatGZ, State) when DatGZ =/= State#state.db ->
+    _ = egeoip:reload({compressed_data, DatGZ}),
+    State#state{ db = DatGZ };
+maybe_reload_db(_DatGZ, State) ->
+    State.
